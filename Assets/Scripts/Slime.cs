8e99b3a1ae7efdcd0be52c7a01e3aea7364817a2
Tachibana446 <stinger33z@gmail.com>
2018-01-13 00:14:36 +0900

    [add]とりあえず攻撃

---
+++ Assets/Scripts/Slime.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+public class Slime : MonoBehaviour
+{
+    /// <summary>
+	/// 味方かどうか
+	/// </summary>
+	public bool isMine = true;
+
+    // Use this for initialization
+    void Start()
+    {
+        var circle = transform.FindChild("Circle");
+        var rate = range / circle.transform.lossyScale.x;
+        circle.localScale = new Vector3(rate, rate, 1);
+    }
+
+    private int frame = 0;
+    private float range = 3.0f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Move
+        float speed = 0.01f;
+        if (!isMine)
+            speed *= -1;
+        var pos = transform.position;
+        pos.x += speed;
+        transform.position = pos;
+        // Attack
+        if (frame % 10 == 0)
+        {
+            int damage = 10;
+
+            GameObject enemy = null;
+            float? distance = null;
+            string targetTag = isMine ? "enemy" : "mine";   // 倒すべき相手のタグ
+            foreach (var e in GameObject.FindGameObjectsWithTag(targetTag))
+            {
+                // 距離
+                var nowDistance = Mathf.Sqrt(Mathf.Pow((e.transform.position.x - pos.x), 2) + Mathf.Pow((e.transform.position.y - pos.y), 2));
+                // 範囲内
+                if (nowDistance > range)
+                    continue;
+                // 距離の小さいものを保持
+                if (distance == null || nowDistance < distance)
+                {
+                    distance = nowDistance;
+                    enemy = e;
+                }
+            }
+            // ターゲットが居れば
+            if (enemy != null)
+            {
+                // Hit処理
+                Debug.Log("Attack!!");
+
+                var u = enemy.GetComponent<Unit>();
+                u.Hp -= damage;
+                if (u.Hp <= 0)
+                {
+                    // TODO: GetMoney
+                    Destroy(enemy);
+                }
+            }
+        }
+
+        frame++;
+    }
+}
---
+++ Assets/Scripts/Unit.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Unit : MonoBehaviour {
+
+	public int Hp = 300;
+    /// <summary>
+    /// 倒した時の獲得賞金
+    /// </summary>
+    public int Money = 100;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
