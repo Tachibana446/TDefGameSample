253886adc3a781d0cb39a5ba69a168b8c8cdc01a
Tachibana446 <stinger33z@gmail.com>
2018-01-13 10:41:16 +0900

    [add]ユニットのHPを表示
    Canvas->Textをアタッチし、そのTextにViewHp.csをアタッチする

---
+++ Assets/Scripts/ViewHp.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+/// <summary>
+/// GUI Textにアタッチして、親のHPを表示する
+/// </summary>
+public class ViewHp : MonoBehaviour {
+
+    Unit parent;
+
+	// Use this for initialization
+	void Start () {
+        parent = GetComponentInParent<Unit>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        GetComponent<Text>().text = parent.Hp.ToString();
+
+    }
+}
